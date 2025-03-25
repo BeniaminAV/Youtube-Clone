4d9dd23c71108c759822a2958d4d413317ed2011
BeniaminAV <black_ben5@yahoo.com>
2025-03-25 20:27:10 +0200

    YTC - New MainSection/HomeLayouts components

--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { HomeNavbar } from "../components/home-navbar";
+import { HomdeSidebar } from "../components/home-sidebar";
 
 interface HomeLayoutProps {
   children: React.ReactNode;
@@ -11,7 +12,7 @@ export const HomeLayout = ({ children }: HomeLayoutProps) => {
       <div className="w-full">
         <HomeNavbar />
         <div className="flex min-h-screen pt-[4rem]">
-          {/* <HomeSidebar /> */}
+          <HomdeSidebar />
           <main className="flex-1 overflow-y-auto">{children}</main>
         </div>
       </div>
