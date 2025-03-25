a6eb31809900d09c4d956512c725eef1b8d2b1fd
BeniaminAV <black_ben5@yahoo.com>
2025-03-25 21:55:31 +0200

    YTC - New implementation for protect route and for offer access to user for many pages if user is authenticated.

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,16 +1,27 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { UserCircleIcon } from "lucide-react";
 
 export const AuthButton = () => {
-  // Add different auth states
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 test-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20
+    <>
+      <SignedIn>
+        <UserButton></UserButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 test-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20
           rounded-full shadow-none [&_svg]:size-4"
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
