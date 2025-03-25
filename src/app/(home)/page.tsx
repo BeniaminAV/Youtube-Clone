23e96a23496896f2200363c785ad63e9fa6bc47d
BeniaminAV <black_ben5@yahoo.com>
2025-03-25 20:33:49 +0200

    YTC - New section, PersonalSection component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <Image src="/logo.svg" height={50} width={50} alt="logo"></Image>
-      <p className="text-xl font-semibold tracking-tight">Youtube&Clone</p>
+      <h2>I will import videos in the future</h2>
     </div>
-  )
+  );
 }
